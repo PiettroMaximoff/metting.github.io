7610b21a90c604e68dbe97e6a61b7f3bd2d39b03
Akira <43845932+PiettroMaximoff@users.noreply.github.com>
2021-08-20 15:03:16 +0300

    Uploaded the files

---
+++ js/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+$(document).ready(function () {
+    $('.icon-menu').click(function (event) {
+        $(this).toggleClass('active');
+        $('.menu__body').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+
+});
