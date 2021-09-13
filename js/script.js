57417f7ee2e370d9bdadffe408580bec6d525a2f
Akira <43845932+PiettroMaximoff@users.noreply.github.com>
2021-09-13 13:23:25 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     $('.icon-menu').click(function (event) {
         $(this).toggleClass('active');
         $('.menu__body').toggleClass('active');
-        $('body').toggleClass('lock');
+        $('body').toggleClass('_lock');
     });
 
 });
